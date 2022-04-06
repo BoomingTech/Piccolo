529a140a23931addf593b14cf19cfc051e7e5d0d
Altair-Alpha <zhangyining0626@126.com>
2022-04-06 22:12:35 +0800

    Fix typos

--- engine/source/runtime/core/math/math_marcos.h
+++ engine/source/runtime/core/math/math_marcos.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define POILT_MIN(x, y) (((x) < (y)) ? (x) : (y))
-#define POILT_MAX(x, y) (((x) > (y)) ? (x) : (y))
-#define POILT_PIN(a, min_value, max_value) POILT_MIN(max_value, POILT_MAX(a, min_value))
+#define PILOT_MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define PILOT_MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define PILOT_PIN(a, min_value, max_value) PILOT_MIN(max_value, PILOT_MAX(a, min_value))
 
-#define POILT_VALID_INDEX(idx, range) (((idx) >= 0) && ((idx) < (range)))
-#define POILT_PIN_INDEX(idx, range) POILT_PIN(idx, 0, (range)-1)
+#define PILOT_VALID_INDEX(idx, range) (((idx) >= 0) && ((idx) < (range)))
+#define PILOT_PIN_INDEX(idx, range) PILOT_PIN(idx, 0, (range)-1)
 
-#define POILT_SIGN(x) ((((x) > 0.0f) ? 1.0f : 0.0f) + (((x) < 0.0f) ? -1.0f : 0.0f))
+#define PILOT_SIGN(x) ((((x) > 0.0f) ? 1.0f : 0.0f) + (((x) < 0.0f) ? -1.0f : 0.0f))
