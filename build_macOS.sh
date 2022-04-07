cc3b78078c9da8af717157981e8eb76fb59061ed
WANG Lei <wlbksy@126.com>
2022-04-07 22:35:31 +0800

    feature: add macOS script too

---
+++ build_macOS.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if test \( $# -ne 1 \);
+then
+    echo "Usage: build.sh arch config"
+    echo ""
+    echo "Configs:"
+    echo "  debug   -   build with the debug configuration"
+    echo "  release -   build with the release configuration"
+    echo ""
+    exit 1
+fi
+
+
+if test \( \( -n "$1" \) -a \( "$1" = "debug" \) \);then
+    CONFIG=" Debug"
+elif test \( \( -n "$1" \) -a \( "$1" = "release" \) \);then
+    CONFIG=" Release"
+else
+    echo "The config \"$1\" is not supported!"
+    echo ""
+    echo "Configs:"
+    echo "  debug   -   build with the debug configuration"
+    echo "  release -   build with the release configuration"
+    echo ""
+    exit 1
+fi
+
+cmake -S . -B build -G "Xcode"
+
+cmake --build build --config "${CONFIG}"
