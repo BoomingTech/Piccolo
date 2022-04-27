236352452ca09dc843dcb6f7aa917a99b2efc36b
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-04-27 21:15:21 +0800

    Fix config_manager.h
    防止创建多个实例.

--- engine/source/runtime/resource/config_manager/config_manager.h
+++ engine/source/runtime/resource/config_manager/config_manager.h
@@ -10,12 +10,6 @@ namespace Pilot
     {
         friend class PublicSingleton<ConfigManager>;
 
-    public:
-        ConfigManager(const ConfigManager&) = delete;
-        ConfigManager& operator=(const ConfigManager&) = delete;
-        //
-        ConfigManager() = default;
-
     private:
         std::filesystem::path m_root_folder;
         std::filesystem::path m_asset_folder;
@@ -25,6 +19,9 @@ namespace Pilot
         std::filesystem::path m_editor_small_icon_path;
         std::filesystem::path m_editor_font_path;
         std::filesystem::path m_global_rendering_res_path;
+
+    protected:
+        ConfigManager() = default;
 
     public:
         void initialize(const EngineInitParams& init_param);
