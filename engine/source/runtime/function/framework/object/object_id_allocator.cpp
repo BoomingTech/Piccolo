268052370ff78b9caf3795108fa532abce90491c
yuanjay07 <algnie@163.com>
2026-07-14 01:44:54 +0800

    优化：使用松散内存序自增并增加溢出检测的中文注释

--- engine/source/runtime/function/framework/object/object_id_allocator.cpp
+++ engine/source/runtime/function/framework/object/object_id_allocator.cpp
@@ -8,7 +8,11 @@ namespace Piccolo
 
     GObjectID ObjectIDAllocator::alloc()
     {
-        GObjectID new_object_ret = m_next_id++;
+        // 使用松散内存顺序（std::memory_order_relaxed）执行原子自增并返回旧值，性能更优
+        GObjectID new_object_ret = m_next_id.fetch_add(1, std::memory_order_relaxed);
+        
+        // 由于 new_object_ret 是自增前的值，判断自增后的值（new_object_ret + 1）是否溢出，
+        // 等价于判断 new_object_ret 是否大于等于 k_invalid_gobject_id - 1
         if (new_object_ret >= k_invalid_gobject_id - 1)
         {
             LOG_FATAL("gobject id overflow");
@@ -18,6 +22,7 @@ namespace Piccolo
     }
 
 } // namespace Piccolo
+
 
 
 
